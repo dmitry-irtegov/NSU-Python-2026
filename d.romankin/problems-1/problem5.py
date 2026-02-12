e5d9416176698d4f475901db47836523534dc537
dromankin <d.romankin@g.nsu.ru>
2026-02-12 17:01:05 +0700

    d.romankin problems1-5

--- d.romankin/problems-1/problem5.py
+++ d.romankin/problems-1/problem5.py
@@ -1,18 +1,16 @@
 def factorize(n : int) -> list[list]:
     factors = []
-    d = 2
-    temp = n
-    
-    while d * d <= temp:
-        if temp % d == 0:
+    d = 2    
+    while d * d <= n:
+        if n % d == 0:
             count = 0
-            while temp % d == 0:
+            while n % d == 0:
                 count += 1
-                temp //= d
+                n //= d
             factors.append([d, count])
         d += 1
     
-    if temp > 1:
-        factors.append([temp, 1])
+    if n > 1:
+        factors.append([n, 1])
         
     return factors
