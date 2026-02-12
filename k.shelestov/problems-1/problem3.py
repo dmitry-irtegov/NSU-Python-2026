3dadf29f95810d3f17ec18da891b208fcc019cd7
Kirill <k.shelestov@g.nsu.ru>
2026-02-12 13:36:15 +0700

    shelestov problems_1_3

---
+++ k.shelestov/problems-1/problem3.py
@@ -0,0 +1,11 @@
+def collatz_sequence(n):
+    while(n!=1):
+        print(n, end='->')
+        if(n%2==0):
+            n=n//2
+        else:
+            n=3*n+1
+    print(1)
+
+n=int(input())
+collatz_sequence(n)
