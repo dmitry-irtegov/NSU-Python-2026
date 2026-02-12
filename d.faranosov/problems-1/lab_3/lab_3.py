b6704c2c92e4aacd1e21b322d9b9c1eb0c1bcb93
system11cls <d.faranosov@g.nsu.ru>
2026-02-12 17:13:33 +0700

    task_1_3

---
+++ d.faranosov/problems-1/lab_3/lab_3.py
@@ -0,0 +1,30 @@
+from sys import argv, stderr
+
+def hyp(num):
+    if (num < 1):
+        raise ValueError
+    nums = [num]
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+            nums.append(num)
+        else:
+            num = num * 3 +1
+            nums.append(num)
+
+    for temp in nums[:-1]:
+        print(f'{temp}->', end="")
+    print(f'{nums[-1]}')
+
+
+if __name__ == '__main__':
+
+    if len(argv) < 2:
+        print("No num to check", file=stderr)
+        exit(1)
+
+    try:
+        hyp(int(argv[1]))
+    except ValueError:
+        print("Invalid number", file=stderr)
+        exit(1)
