1e7047a152b476276c779b921f0b4dde6c09e899
system1cls <d.faranosov@gmail.com>
2026-02-16 15:08:20 +0700

    added tests

--- d.faranosov/problems-1/lab_2/lab_2.py
+++ d.faranosov/problems-1/lab_2/lab_2.py
@@ -2,5 +2,3 @@ def bounds(lower_bound, upper_bound, nums):
     res = map(lambda x: lower_bound if x < lower_bound else upper_bound if x > upper_bound else x, nums)
     return list(res)
 
-assert (bounds(1, 3, [0, 2, 4]) == [1, 2, 3])
-assert (bounds(0, 0, [1, -1, 5]) == [0, 0, 0])
