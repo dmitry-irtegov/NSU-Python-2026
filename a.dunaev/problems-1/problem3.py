50611b05b9f9e4e4ced0c63ded3e78e49998c222
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2026-02-18 18:35:28 +0700

    added tests

--- a.dunaev/problems-1/problem3.py
+++ a.dunaev/problems-1/problem3.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python
 
-inp = int(input())
+orig_print = print
+buffer = ""
 
-while inp > 1:
-    print(inp, end=" -> ")
-    inp = inp // 2 if inp % 2 == 0 else 3 * inp + 1
+def custom_print(*args, end="\n"):
+    global buffer
 
-print(inp)
+    for i in args:
+        buffer += str(i) + end
+
+print = custom_print
+
+def program(inp: int) -> None:
+    while inp > 1:
+        print(inp, end=" -> ")
+        inp = inp // 2 if inp % 2 == 0 else 3 * inp + 1
+
+    print(inp)
+
+def test():
+    from random import randint
+
+    def test_case():
+        num = randint(1, 100)
+        program(num)
+        assert buffer[-2] == "1"
+
+    for i in range(100):
+        test_case()
+
+if __name__ == "__main__":
+    test()
+
+print = orig_print
