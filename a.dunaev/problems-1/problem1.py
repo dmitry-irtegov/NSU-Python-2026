54c285acabab2ac90ded0eb7cc8de71b73edfafe
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2026-02-18 18:22:13 +0700

    added tests

--- a.dunaev/problems-1/problem1.py
+++ a.dunaev/problems-1/problem1.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
+
+
 def compute(inp: list) -> list:
     out = [0]
     for i in inp:
         out.append(out[-1] + i)
     return out
 
+
+def test():
+    from random import randint
+
+    def test_case():
+        arr = [randint(-100, 100) for _ in range(100)]
+        new_arr = compute(arr)
+        original_arr = []
+        for i in range(len(new_arr)-1):
+            original_arr.append(new_arr[i+1] - new_arr[i])
+        assert  original_arr == arr
+
+    for i in range(100):
+        test_case()
+
 if __name__ == "__main__":
-    inp = [1, 2, 3]
-    assert compute(inp) == [0, 1, 3, 6]
-
-    inp = []
-    assert compute(inp) == [0]
-
-    inp = [4, 6, 4, 3, 2]
-    assert compute(inp) == [0, 4, 10, 14, 17, 19]
+    test()
