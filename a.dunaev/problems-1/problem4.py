e601168aaf3513d7c6b2c209c31b999fe34bc3f7
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2026-02-18 15:52:26 +0700

    task 1-4

---
+++ a.dunaev/problems-1/problem4.py
@@ -0,0 +1,7 @@
+nums = ["no", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+n = 10
+while n != 0:
+    print(f"{nums[n].capitalize()} green bottles sitting on the wall,\n" * 2 +
+          "And if one green bottle should accidentally fall,\n" +
+          f"There’ll be {nums[n-1]} green bottles sitting on the wall.\n")
+    n -= 1
