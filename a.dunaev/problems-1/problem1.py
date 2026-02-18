6bf2226b5ca5e17dffab9625a13eb1b14c685acd
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2026-02-18 17:48:59 +0700

    updated

--- a.dunaev/problems-1/problem1.py
+++ a.dunaev/problems-1/problem1.py
@@ -3,3 +3,13 @@ def compute(inp: list) -> list:
     for i in inp:
         out.append(out[-1] + i)
     return out
+
+if __name__ == "__main__":
+    inp = [1, 2, 3]
+    assert compute(inp) == [0, 1, 3, 6]
+
+    inp = []
+    assert compute(inp) == [0]
+
+    inp = [4, 6, 4, 3, 2]
+    assert compute(inp) == [0, 4, 10, 14, 17, 19]
