5f03b81a6980f0a62ec620a4907d5ae5b4a126e5
Nikita Krainov <n.krainov@g.nsu.ru>
2026-02-19 12:34:49 +0700

    added input from stdin

--- n.krainov/problems1/problem1.py
+++ n.krainov/problems1/problem1.py
@@ -11,3 +11,30 @@ def cumulative_sum(numList):
 assert cumulative_sum([10, 20, 30]) == [0, 10, 30, 60]
 assert cumulative_sum([0]) == [0, 0]
 assert cumulative_sum([]) == [0]
+
+if __name__ == '__main__':
+    listLen = input("Please enter a list length: ")
+    listNums = []
+
+    if not listLen.isdigit():
+        print("It's not a number")
+        exit(1)
+
+    listLen = int(listLen)
+
+    if listLen < 0:
+        print("List can't have negative length")
+        exit(1)
+
+    if listLen != 0:
+        print("Please enter list numbers")
+
+    for i in range(listLen):
+        num = input()
+        if not num.isdigit():
+            print("It's not a number")
+            exit(1)
+
+        listNums.append(int(num))
+
+    print(cumulative_sum(listNums))
