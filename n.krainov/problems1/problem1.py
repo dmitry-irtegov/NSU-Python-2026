c223bf9750a190e622099396a07c9f5a7e0448f2
Nikita Krainov <n.krainov@g.nsu.ru>
2026-02-19 13:01:08 +0700

    changed error handling

--- n.krainov/problems1/problem1.py
+++ n.krainov/problems1/problem1.py
@@ -13,28 +13,24 @@ assert cumulative_sum([0]) == [0, 0]
 assert cumulative_sum([]) == [0]
 
 if __name__ == '__main__':
-    listLen = input("Please enter a list length: ")
-    listNums = []
+    try:
+        listLen = input("Please enter a list length: ")
+        listNums = []
 
-    if not listLen.isdigit():
-        print("It's not a number")
-        exit(1)
+        listLen = int(listLen)
 
-    listLen = int(listLen)
-
-    if listLen < 0:
-        print("List can't have negative length")
-        exit(1)
-
-    if listLen != 0:
-        print("Please enter list numbers")
-
-    for i in range(listLen):
-        num = input()
-        if not num.isdigit():
-            print("It's not a number")
+        if listLen < 0:
+            print("List can't have negative length")
             exit(1)
 
-        listNums.append(int(num))
+        if listLen != 0:
+            print("Please enter list numbers")
 
-    print(cumulative_sum(listNums))
+        for i in range(listLen):
+            num = input()
+            listNums.append(int(num))
+
+        print(cumulative_sum(listNums))
+    except ValueError as e:
+        print("error occurred: " + str(e))
+        exit(1)
