9cef1dd74ea5044300a3e1b50589b5b64f73946a
MCBivis <Artemiybox@mail.ru>
2026-02-21 13:45:50 +0700

    fix

--- a.kuleshov/problems-1/problem3.py
+++ a.kuleshov/problems-1/problem3.py
@@ -1,14 +1,14 @@
-n = int(input("Введите число: "))
+if __name__ == "__main__":
+    n = int(input())
 
-sequence = [n]
+    sequence = [n]
 
-while n != 1:
-    if n % 2 == 0:
-        n = n // 2
-    else:
-        n = 3 * n + 1
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
 
-    sequence.append(n)
+        sequence.append(n)
 
-print("Цепочка Коллатца:")
-print(*sequence)
+    print(*sequence, sep=" -> ")
