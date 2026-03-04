01791f3f9ae7514529044af5a75e571826476230
Takumy <Talumy@duck.com>
2026-03-04 22:42:39 +0700

    unit test added

--- i.pereverzev2/problems1/problem3.py
+++ i.pereverzev2/problems1/problem3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+import unittest
+import sys
+from io import StringIO
 
 
 def collatc(num):
@@ -11,5 +14,31 @@ def collatc(num):
         print(" -> ", num, end="")
 
 
+class TestCollatc(unittest.TestCase):
+
+    def test_output(self):
+        original_stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        collatc(5)
+
+        output = sys.stdout.getvalue()
+        sys.stdout = original_stdout
+
+        expected = "5 ->  16 ->  8 ->  4 ->  2 ->  1"
+        self.assertEqual(output, expected)
+
+    def test_one(self):
+        original_stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        collatc(1)
+
+        output = sys.stdout.getvalue()
+        sys.stdout = original_stdout
+
+        self.assertEqual(output, "1")
+
+
 if __name__ == "__main__":
-    collatc(int(input()))
+    unittest.main()
