e2823b7681aabde451cf6f65c3c0aa0e06202acc
Takumy <Talumy@duck.com>
2026-03-05 16:27:22 +0700

    unit test added

--- i.pereverzev2/problems1/problem4.py
+++ i.pereverzev2/problems1/problem4.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+import unittest
+import sys
+from io import StringIO
 
 
 def song():
@@ -24,5 +27,60 @@ def song():
         print(f"There'll be {lower_num} {part}.")
 
 
+class TestSong(unittest.TestCase):
+
+    def test_song(self):
+        original_stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        song()
+
+        output = sys.stdout.getvalue()
+        sys.stdout = original_stdout
+
+        expected = """Ten green bottles hanging on the wall,
+Ten green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be nine green bottles hanging on the wall.
+Nine green bottles hanging on the wall,
+Nine green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be eight green bottles hanging on the wall.
+Eight green bottles hanging on the wall,
+Eight green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be seven green bottles hanging on the wall.
+Seven green bottles hanging on the wall,
+Seven green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be six green bottles hanging on the wall.
+Six green bottles hanging on the wall,
+Six green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be five green bottles hanging on the wall.
+Five green bottles hanging on the wall,
+Five green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be four green bottles hanging on the wall.
+Four green bottles hanging on the wall,
+Four green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be three green bottles hanging on the wall.
+Three green bottles hanging on the wall,
+Three green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be two green bottles hanging on the wall.
+Two green bottles hanging on the wall,
+Two green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be one green bottles hanging on the wall.
+One green bottles hanging on the wall,
+One green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be no green bottles hanging on the wall.
+"""
+        self.assertEqual(output, expected)
+
+
 if __name__ == "__main__":
-    song()
+    unittest.main()
