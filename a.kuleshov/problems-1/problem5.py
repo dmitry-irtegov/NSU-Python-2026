3279bb0d0a9105047a1ec62df902f5bca7517e3e
MCBivis <Artemiybox@mail.ru>
2026-03-07 17:07:45 +0700

    add test

--- a.kuleshov/problems-1/problem5.py
+++ a.kuleshov/problems-1/problem5.py
@@ -1,3 +1,5 @@
+import unittest
+
 def prime_factors(n):
     factors = []
     d = 2
@@ -19,8 +21,23 @@ def prime_factors(n):
 
     return factors
 
-num = int(input("Введите число: "))
+class TestPrimeFactors(unittest.TestCase):
 
-result = prime_factors(num)
+    def test_prime_number(self):
+        self.assertEqual(prime_factors(13), [[13, 1]])
 
-print(result)
+    def test_composite_number(self):
+        self.assertEqual(prime_factors(12), [[2, 2], [3, 1]])
+
+    def test_power_of_prime(self):
+        self.assertEqual(prime_factors(16), [[2, 4]])
+
+    def test_two_primes(self):
+        self.assertEqual(prime_factors(15), [[3, 1], [5, 1]])
+
+    def test_one(self):
+        self.assertEqual(prime_factors(1), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
