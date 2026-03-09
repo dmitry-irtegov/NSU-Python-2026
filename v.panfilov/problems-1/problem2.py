7b799752df04abc36789de7beaf3c9a4576ba6a8
Sewvka <hoteroke@gmail.com>
2026-03-09 13:38:48 +0700

    fix linter

--- v.panfilov/problems-1/problem2.py
+++ v.panfilov/problems-1/problem2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3.12
-from typing import List
 import unittest
 from sys import stderr, exit
 
@@ -30,27 +29,30 @@ class TestFltr(unittest.TestCase):
     def test_float_values(self):
         self.assertEqual(fltr([1.5, 2.7, 3.2, 4.8], 2.0, 4.0), [2.0, 2.7, 3.2, 4.0])
 
+    def test_same_border(self):
+        self.assertEqual(fltr([1, 2, 3, 4, 5, 6, 7], 3, 3), [3, 3, 3, 3, 3, 3, 3])
+
     
 
 if __name__ == '__main__':
     while True:
         try:
-            l = input('Enter the list: ')
+            list = input('Enter the list: ')
             a = int(input('lower bound: '))
             b = int(input('upper bound: '))
-            result = fltr(map(int, l.split()), a, b)
+            result = fltr(map(int, list.split()), a, b)
             print('Answer for the list is: ', result)
             break
         except ValueError as e:
             print(f'Неверные входные данные:\n{e}', file=stderr)
         except KeyboardInterrupt:
-            print("KeyboardInterrupt received")
+            print("KeyboardInterrupt received", file=stderr)
             exit()
         except EOFError:
-            print("EOFError received")
+            print("EOFError received", file=stderr)
             exit()
         except Exception as e:
-            print(f"Ошибка:\n {e}")
+            print(f"Ошибка:\n {e}", file=stderr)
             exit()
     print('Тесты:')
     unittest.main()
