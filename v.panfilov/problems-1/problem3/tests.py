82855dfad4aea0746476a716af9c006b1f9db59e
Sewvka <hoteroke@gmail.com>
2026-03-12 19:05:11 +0700

    split tests and main

--- v.panfilov/problems-1/problem3/tests.py
+++ v.panfilov/problems-1/problem3/tests.py
@@ -1,27 +1,5 @@
-#!/usr/bin/env python3.12
-
+from problem3 import collatz_sequence
 import unittest
-from sys import stderr, exit
-
-
-def collatz_sequence(n: int):
-    """
-    Возвращает последовательность Коллатца для заданного числа n.
-    Последовательность заканчивается числом 1.
-    """
-    if n <= 0:
-        raise ValueError("Число должно быть положительным")
-    seq = [n]
-    try:
-        while n != 1:
-            if n % 2 == 0:
-                n = n // 2
-            else:
-                n = 3 * n + 1
-            seq.append(n)
-        return seq
-    except ValueError as e:
-        print(f"Ошибка: {e}")
 
 
 class TestСollatz(unittest.TestCase):
@@ -38,25 +16,5 @@ class TestСollatz(unittest.TestCase):
         self.assertRaises(ValueError, collatz_sequence, -1)
 
 
-if __name__ == '__main__':
-    """Основная функция программы."""
-    while True:
-        try:
-            user_input = input("Введите натуральное число: ")
-            n = int(user_input)
-            sequence = collatz_sequence(n)
-            print(" → ".join(map(str, sequence)))
-            break
-        except ValueError as e:
-            print(f"Ошибка: {e}", file=stderr)
-        except KeyboardInterrupt:
-            print("KeyboardInterrupt received", file=stderr)
-            exit()
-        except EOFError:
-            print("EOFError received", file=stderr)
-            exit()
-        except Exception as e:
-            print(f"Ошибка:\n {e}", file=stderr)
-            exit()
-    print("Тесты:")
+if __name__ == "__main__":
     unittest.main()
