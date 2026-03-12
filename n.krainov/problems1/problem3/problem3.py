6ba34bd951bb3da6181b81854b7d5db40ba566e0
Nikita Krainov <n.krainov@g.nsu.ru>
2026-03-12 18:59:51 +0700

    removed print and isDigit

--- n.krainov/problems1/problem3/problem3.py
+++ n.krainov/problems1/problem3/problem3.py
@@ -1,10 +1,11 @@
-def collatz_seq(number, maxIter = 1 << 61):
+def print_collatz_seq(number, maxIter = 1 << 61):
     seq = [number]
     n = number
     i = 0
     while n != 1:
         if i == maxIter:
-            return False, []
+            print("[]")
+            return
 
         if n % 2 == 0:
             n = n // 2
@@ -14,30 +15,17 @@ def collatz_seq(number, maxIter = 1 << 61):
         i += 1
         seq.append(n)
 
-    return True, seq
+    print(' -> '.join(str(n) for n in seq))
 
-original_print = print
-def print(*args, **kwargs):
-    new_args = []
-    for arg in args:
-        if isinstance(arg, tuple) and len(arg) == 2:
-            first, second = arg
-            if isinstance(first, bool) and isinstance(second, list):
-                new_args.append(str(first) + " " + ' -> '.join(str(x) for x in second))
-            else:
-                new_args.append(arg)
-        else:
-            new_args.append(arg)
-    original_print(*new_args, **kwargs)
 
 if __name__ == '__main__':
     import sys
 
-    num = input("Enter a number: ")
+    try:
+        num = input("Enter a number: ")
 
-    if num.isdigit():
         num = int(num)
-        print(collatz_seq(num))
-    else:
-        sys.stderr.write("it isn't a number")
+        print_collatz_seq(num)
+    except Exception as e:
+        sys.stderr.write("Error occurred: " + str(e))
         exit(1)
--- n.krainov/problems1/problem3/tests.py
+++ n.krainov/problems1/problem3/tests.py
@@ -1,53 +1,47 @@
 import unittest
-from problem3 import collatz_seq
+from unittest.mock import patch
+import io
+from problem3 import print_collatz_seq
+
+
+def get_output(number, maxIter=1 << 61):
+    with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        print_collatz_seq(number, maxIter)
+        return mock_stdout.getvalue().strip()
+
 
 class TestCollatzSeqKnownValues(unittest.TestCase):
     def test_number_1(self):
-        flag, seq = collatz_seq(1)
-        self.assertTrue(flag)
-        self.assertEqual(seq, [1])
+        self.assertEqual(get_output(1), '1')
 
     def test_number_2(self):
-        flag, seq = collatz_seq(2)
-        self.assertTrue(flag)
-        self.assertEqual(seq, [2, 1])
+        self.assertEqual(get_output(2), '2 -> 1')
 
     def test_number_3(self):
-        flag, seq = collatz_seq(3)
-        self.assertTrue(flag)
-        self.assertEqual(seq, [3, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(get_output(3), '3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1')
 
     def test_number_6(self):
-        flag, seq = collatz_seq(6)
-        self.assertTrue(flag)
-        self.assertEqual(seq, [6, 3, 10, 5, 16, 8, 4, 2, 1])
+        self.assertEqual(get_output(6), '6 -> 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1')
 
     def test_number_27_length(self):
-        flag, seq = collatz_seq(27)
-        self.assertTrue(flag)
-        self.assertEqual(len(seq), 112)
-
+        output = get_output(27)
+        self.assertEqual(len(output.split(' -> ')), 112)
 
 
 class TestCollatzSeqMaxIter(unittest.TestCase):
-    def test_exceeds_max_iter_returns_false(self):
-        flag, seq = collatz_seq(3, maxIter=3)
-        self.assertFalse(flag)
-        self.assertEqual(seq, [])
+    def test_exceeds_max_iter(self):
+        self.assertEqual(get_output(3, maxIter=3), '[]')
 
-    def test_exceeds_max_iter_returns_empty_list(self):
-        _, seq = collatz_seq(3, maxIter=1)
-        self.assertIsInstance(seq, list)
-        self.assertEqual(len(seq), 0)
+    def test_exceeds_max_iter_single(self):
+        self.assertEqual(get_output(3, maxIter=1), '[]')
 
     def test_exact_max_iter_completes(self):
-        flag, seq = collatz_seq(2, maxIter=1)
-        self.assertTrue(flag)
-        self.assertEqual(seq, [2, 1])
+        self.assertEqual(get_output(2, maxIter=1), '2 -> 1')
 
     def test_default_max_iter_large_number(self):
-        flag, _ = collatz_seq(1_000_000)
-        self.assertTrue(flag)
+        output = get_output(1_000_000)
+        self.assertTrue(output.endswith('-> 1'))
+
 
 if __name__ == '__main__':
     unittest.main()
