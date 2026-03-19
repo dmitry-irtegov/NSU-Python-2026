febafd897cdfd8fea3000e55e8b2bfa83c6c74a1
Nikita Krainov <n.krainov@g.nsu.ru>
2026-03-19 14:50:02 +0700

    two appends -> cycle, two dicts -> one dict

--- n.krainov/problems1/problem4/problem4.py
+++ n.krainov/problems1/problem4/problem4.py
@@ -1,14 +1,11 @@
 def print_ten_green_bottles():
     parts = []
-    numToWordUppercase = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
+    numToWordUppercase = {0: "No", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
                           6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"}
 
-    numToWordLastLine = {0: "no", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five",
-                         6: "six", 7: "seven", 8: "eight", 9: "nine"}
-
     for i in range(10, 0, -1):
-        parts.append(f"{numToWordUppercase[i]} green bottles hanging on the wall,\n")
-        parts.append(f"{numToWordUppercase[i]} green bottles hanging on the wall,\n")
+        for _ in range(2):
+            parts.append(f"{numToWordUppercase[i]} green bottles hanging on the wall,\n")
 
         if i == 1:
             parts.append("If that ")
@@ -16,7 +13,7 @@ def print_ten_green_bottles():
             parts.append("And if ")
 
         parts.append("one green bottle should accidentally fall\n")
-        parts.append(f"There'll be {numToWordLastLine[i - 1]} green bottles hanging on the wall.\n")
+        parts.append(f"There'll be {numToWordUppercase[i - 1].lower()} green bottles hanging on the wall.\n")
 
     print(''.join(parts))
 
