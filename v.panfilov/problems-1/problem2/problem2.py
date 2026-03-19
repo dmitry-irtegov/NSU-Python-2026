a6bbf8ae02e5d46494d94c3afba98dee6d2c53ac
Sewvka <46619541+Sewvka@users.noreply.github.com>
2026-03-19 18:24:08 +0700

    second problem (#48)
    * second problem
    
    * exceptions added
    
    * fix linter
    
    * split tests and main
    
    * fix

---
+++ v.panfilov/problems-1/problem2/problem2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+from sys import stderr, exit
+
+
+def fltr(list, a: int, b: int):
+    if b < a:
+        raise ValueError("Верхний лимит должен быть больше нижнего")
+    result = [max(min(elem, b), a) for elem in list]
+    return result
+    
+
+if __name__ == '__main__':
+    while True:
+        try:
+            list = input('Enter the list: ')
+            a = int(input('lower bound: '))
+            b = int(input('upper bound: '))
+            result = fltr(map(int, list.split()), a, b)
+            print('Answer for the list is: ', result)
+            break
+        except ValueError as e:
+            print(f'Неверные входные данные:\n{e}', file=stderr)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt received", file=stderr)
+            exit()
+        except EOFError:
+            print("EOFError received", file=stderr)
+            exit()
+        except Exception as e:
+            print(f"Ошибка:\n {e}", file=stderr)
+            exit()
+            
