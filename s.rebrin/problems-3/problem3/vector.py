392bf4ff71c43114b0fe10dff8c07e8a1a4403c0
Sergei Rebrin <s.rebrin@g.nsu.ru>
2026-03-26 13:18:04 +0700

    problem3-3

---
+++ s.rebrin/problems-3/problem3/vector.py
@@ -0,0 +1,74 @@
+from math import sqrt
+from typing import Iterable
+
+
+class Vector:
+    def __init__(self, arr: Iterable[int]):
+        arr = list(arr)
+        if not all(isinstance(x, int) for x in arr):
+            raise TypeError("All elements must be integers")
+        self.arr = arr
+
+    def __len__(self) -> int:
+        return len(self.arr)
+
+    def __add__(self, other: object) -> "Vector":
+        if isinstance(other, Vector):
+            if len(self) != len(other):
+                raise ValueError("Vectors must be the same size")
+            return Vector(a + b for a, b in zip(self.arr, other.arr))
+        elif isinstance(other, int):
+            return Vector(a + other for a in self.arr)
+        return NotImplemented
+
+    def __radd__(self, other: int) -> "Vector":
+        if isinstance(other, int):
+            return self + other
+        return NotImplemented
+
+    def __sub__(self, other: object) -> "Vector":
+        if isinstance(other, Vector):
+            if len(self) != len(other):
+                raise ValueError("Vectors must be the same size")
+            return Vector(a - b for a, b in zip(self.arr, other.arr))
+        elif isinstance(other, int):
+            return Vector(a - other for a in self.arr)
+        return NotImplemented
+
+    def __rsub__(self, other: int) -> "Vector":
+        if isinstance(other, int):
+            return Vector(other - a for a in self.arr)
+        return NotImplemented
+
+    def __mul__(self, other: int) -> "Vector":
+        if isinstance(other, int):
+            return Vector(a * other for a in self.arr)
+        return NotImplemented
+
+    def __rmul__(self, other: int) -> "Vector":
+        if isinstance(other, int):
+            return self * other
+        return NotImplemented
+
+    def dot(self, other: "Vector") -> int:
+        if not isinstance(other, Vector):
+            raise TypeError("Expected Vector")
+        if len(self) != len(other):
+            raise ValueError("Vectors must be the same size")
+        return sum(a * b for a, b in zip(self.arr, other.arr))
+
+    def length(self) -> float:
+        return sqrt(sum(a * a for a in self.arr))
+
+    def __getitem__(self, i: int) -> int:
+        if not isinstance(i, int):
+            raise TypeError("Index must be int")
+        return self.arr[i]
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Vector):
+            return NotImplemented
+        return self.arr == other.arr
+
+    def __repr__(self) -> str:
+        return f"Vector({self.arr})"
