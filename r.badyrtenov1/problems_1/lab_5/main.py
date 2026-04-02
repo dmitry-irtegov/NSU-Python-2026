6b96d366c30ba78af3f4323e622ac02033de1d6a
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-04-02 14:23:40 +0700

    problem_1_5

---
+++ r.badyrtenov1/problems_1/lab_5/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from sys import stderr
+
+
+def factorize(num):
+    div = 2
+    res = []
+    exp = 0
+    while num > 1:
+        if num % div == 0:
+            num //= div
+            exp += 1
+        else:
+            if exp != 0:
+                res.append([div, exp])
+                exp = 0
+            div += 1
+    if exp != 0:
+        res.append([div, exp])
+    return res
+
+
+if __name__ == "__main__":
+    try:
+        n = int(input("Enter number: "))
+        print(factorize(n))
+    except KeyboardInterrupt:
+        print("Shutting down...")
+    except Exception as e:
+        print(f"Exception in main(): {e}", file=stderr)
