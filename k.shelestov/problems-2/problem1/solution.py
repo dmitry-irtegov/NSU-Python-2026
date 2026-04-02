6af9184125749be2288d08b5944bb9af2f9e4ff6
Kirill <k.shelestov@g.nsu.ru>
2026-04-02 10:39:59 +0700

    fix type

--- k.shelestov/problems-2/problem1/solution.py
+++ k.shelestov/problems-2/problem1/solution.py
@@ -1,14 +1,14 @@
 from math import gcd
 
 
-def primitive_pythagorean_triples(limit: int):
-    triples = [
-        tuple(sorted((m*m - n*n, 2*m*n, m*m + n*n)))
-        for m in range(2, int(limit**0.5) + 2)
-        for n in range(1, m)
-        if gcd(m, n) == 1
-        and (m - n) % 2 == 1
-        and m*m + n*n <= limit
-    ]
+def primitive_pythagorean_triples(limit: int) -> list[tuple[int, int, int]]:
+    triples: list[tuple[int, int, int]] = []
+
+    for m in range(2, int(limit ** 0.5) + 2):
+        for n in range(1, m):
+            if gcd(m, n) == 1 and (m - n) % 2 == 1:
+                if m * m + n * n <= limit:
+                    a, b, c = sorted((m*m - n*n, 2*m*n, m*m + n*n))
+                    triples.append((a, b, c))  
 
     return sorted(triples)
