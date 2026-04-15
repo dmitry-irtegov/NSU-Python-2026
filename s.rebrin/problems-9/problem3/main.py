4a0551108d0016451a293d56d52d0517b74b8901
Sergei Rebrin <s.rebrin@g.nsu.ru>
2026-04-16 00:25:32 +0700

    problem-9-3

---
+++ s.rebrin/problems-9/problem3/main.py
@@ -0,0 +1,32 @@
+class Directory:
+    def __getattribute__(self, name):
+        attr = object.__getattribute__(self, name)
+
+        if callable(attr) and not name.startswith("__"):
+
+            def wrapper(*args, **kwargs):
+                d = object.__getattribute__(self, "__dict__")
+
+                if "m" not in d:
+                    d["m"] = {}
+
+                if name not in d["m"]:
+                    d["m"][name] = {"count": 0, "calls": []}
+
+                d["m"][name]["count"] += 1
+                d["m"][name]["calls"].append({"args": args, "kwargs": kwargs})
+
+                return attr(*args, **kwargs)
+
+            return wrapper
+
+        return attr
+
+    def __str__(self):
+        d = self.__dict__
+        return str(d.get("m", {}))
+
+
+d = Directory()
+d.__str__()
+print(str(d))
