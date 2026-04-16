e84229cc7c6a202b59959708a7be195759dae2b7
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-04-16 22:49:40 +0700

    fix

--- r.badyrtenov1/problems_2/lab_5/main.py
+++ r.badyrtenov1/problems_2/lab_5/main.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
+from functools import reduce
 from sys import stderr
 
 
 def primes(num):
-    if num <= 1:
-        return []
-
-    sieve = [True] * (num + 1)
-    for i in range(2, int(num ** 0.5) + 1):
-        if sieve[i]:
-            sieve[i * i: num + 1: i] = [False] * len(range(i * i, num + 1, i))
-
-    return [i for i in range(2, num + 1) if sieve[i]]
+    return [i for i, val in enumerate(
+        reduce(
+            lambda s, i: (
+                s if not s[i] else
+                s.__setitem__(slice(i * i, num + 1, i), [False] * len(range(i * i, num + 1, i))) or s
+            ),
+            range(2, int(num ** 0.5) + 1),
+            [True] * (num + 1)
+        )
+    ) if val and i > 1]
 
 
 if __name__ == "__main__":
