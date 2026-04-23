d9be4d700843e420c6dbb7d558e5f49cb843e289
Ramil Badyrtenov <150883678+ramen-badr@users.noreply.github.com>
2026-04-23 12:52:17 +0700

    r.badyrtenov problem_2_2 (#169)
    * r.badyrtenov problem_2_2
    
    * fix for Windows
    
    ---------
    
    Co-authored-by: r.badyrtenov <r.badyrtenov@tbank.ru>

---
+++ r.badyrtenov1/problems_2/lab_2/test_main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import unittest
+from os import path, remove
+from tempfile import NamedTemporaryFile
+
+from main import make_latin_dict
+
+
+class TestCumulativeSums(unittest.TestCase):
+    def setUp(self):
+        self.temp_file = NamedTemporaryFile(delete=False)
+        self.temp_file.close()
+
+    def tearDown(self):
+        if path.exists(self.temp_file.name):
+            remove(self.temp_file.name)
+
+    def write_dict_to_file(self, content):
+        with open(self.temp_file.name, 'w') as f:
+            f.write(content)
+
+    def test_empty(self):
+        self.write_dict_to_file("")
+        self.assertEqual(make_latin_dict(self.temp_file.name), {})
+
+    def test_empty_lines(self):
+        self.write_dict_to_file('''
+
+            apple - malum
+
+            fruit - baca
+
+        ''')
+        self.assertEqual(make_latin_dict(self.temp_file.name), {
+            "malum": ["apple"],
+            "baca": ["fruit"],
+        })
+
+    def test_example(self):
+        self.write_dict_to_file('''
+            apple - malum, pomum, popula
+            fruit - baca, bacca, popum
+            punishment - malum, multa
+        ''')
+        self.assertEqual(make_latin_dict(self.temp_file.name), {
+            "baca": ["fruit"],
+            "bacca": ["fruit"],
+            "malum": ["apple", "punishment"],
+            "multa": ["punishment"],
+            "pomum": ["apple"],
+            "popula": ["apple"],
+            "popum": ["fruit"]
+        })
+
+
+if __name__ == "__main__":
+    unittest.main()
