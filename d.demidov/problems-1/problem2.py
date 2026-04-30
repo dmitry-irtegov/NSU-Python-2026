292a984ddeda352952fa85d729d2bdf90d151d7d
“Dmitriy <d.demidov@g.nsu.ru>
2026-04-30 18:01:10 +0700

    replace pytest with unittest

--- d.demidov/problems-1/problem2.py
+++ d.demidov/problems-1/problem2.py
@@ -1,4 +1,4 @@
-import pytest
+import unittest
 
 
 def clamp_array_to_bounds(arr, lower_bound, upper_bound):
@@ -17,34 +17,31 @@ def clamp_array_to_bounds(arr, lower_bound, upper_bound):
     return result
 
 
-def test_empty():
-    original_list = []
-    result_list = clamp_array_to_bounds(original_list, -10, 10)
-    expected_list = []
-    assert result_list == expected_list
+class TestClampArrayToBounds(unittest.TestCase):
+    def test_empty(self):
+        original_list = []
+        result_list = clamp_array_to_bounds(original_list, -10, 10)
+        expected_list = []
+        self.assertEqual(result_list, expected_list)
 
+    def test_lower_bound(self):
+        original_list = [1, 2, 3, 4, 5]
+        result_list = clamp_array_to_bounds(original_list, 3, 10)
+        expected_list = [3, 3, 3, 4, 5]
+        self.assertEqual(result_list, expected_list)
 
-def test_lower_bound():
-    original_list = [1, 2, 3, 4, 5]
-    result_list = clamp_array_to_bounds(original_list, 3, 10)
-    expected_list = [3, 3, 3, 4, 5]
-    assert result_list == expected_list
+    def test_upper_bound(self):
+        original_list = [1, 2, 3, 4, 5]
+        result_list = clamp_array_to_bounds(original_list, -10, 3)
+        expected_list = [1, 2, 3, 3, 3]
+        self.assertEqual(result_list, expected_list)
 
+    def test_invalid_args(self):
+        original_list = [1, 2, 3, 4, 5]
+        with self.assertRaises(ValueError):
+            clamp_array_to_bounds(original_list, 5, -5)
 
-def test_upper_bound():
-    original_list = [1, 2, 3, 4, 5]
-    result_list = clamp_array_to_bounds(original_list, -10, 3)
-    expected_list = [1, 2, 3, 3, 3]
-    assert result_list == expected_list
-
-
-def test_invalid_args():
-    original_list = [1, 2, 3, 4, 5]
-    with pytest.raises(ValueError):
-        clamp_array_to_bounds(original_list, 5, -5)
-
-
-def test_original_list_unchanged():
-    original_list = [1, 2, 3, 4, 5]
-    clamp_array_to_bounds(original_list, 1, 5)
-    assert original_list == [1, 2, 3, 4, 5]
+    def test_original_list_unchanged(self):
+        original_list = [1, 2, 3, 4, 5]
+        clamp_array_to_bounds(original_list, 1, 5)
+        self.assertEqual(original_list, [1, 2, 3, 4, 5])
