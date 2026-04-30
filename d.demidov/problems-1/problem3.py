52aee07b839c1a55612507e4a5c8610730591928
“Dmitriy <d.demidov@g.nsu.ru>
2026-04-30 17:18:52 +0700

    add -> 1 endline

--- d.demidov/problems-1/problem3.py
+++ d.demidov/problems-1/problem3.py
@@ -9,6 +9,7 @@ def collatz_conjecture_compute(num: int) -> str:
         numbers_chain.append(str(num))
         num = num // 2 if num % 2 == 0 else num * 3 + 1
 
+    numbers_chain.append("1")
     return " -> ".join(numbers_chain)
 
 
@@ -16,7 +17,7 @@ def test_collatz_conjecture_compute() -> None:
     try:
         input_num = 3 if len(sys.argv) < 2 else int(sys.argv[1])
         res = collatz_conjecture_compute(input_num)
-        assert res.endswith(" -> 2")
+        assert res.endswith(" -> 1")
         print(res)
     except Exception as err:
         logger.exception(f"Exception in collatz test: {err}")
