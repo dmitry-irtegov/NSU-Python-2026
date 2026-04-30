9e436596608e2caca0386ff0b4272b8d8aa1474b
“Dmitriy <d.demidov@g.nsu.ru>
2026-04-30 17:37:38 +0700

    add reproduvable tests

--- d.demidov/problems-1/problem2.py
+++ d.demidov/problems-1/problem2.py
@@ -1,29 +1,50 @@
-import uuid
+import pytest
 
 
-def clamp_array_to_bounds(
-        nums: list[int | float],
-        lower_bound: int | float,
-        upper_bound: int | float
-) -> list[int | float]:
-    return [max(lower_bound, min(num, upper_bound)) for num in nums]
+def clamp_array_to_bounds(arr, lower_bound, upper_bound):
+    if lower_bound > upper_bound:
+        raise ValueError("Lower bound must be less than upper bound")
+
+    result = []
+
+    for x in arr:
+        if x > upper_bound:
+            result.append(upper_bound)
+        elif x < lower_bound:
+            result.append(lower_bound)
+        else:
+            result.append(x)
+    return result
 
 
-def test_clamp_array_to_bounds() -> None:
-    random_numbers = [uuid.uuid4().int for _ in range(10)]
-    lower_bound = uuid.uuid4().int
-    upper_bound = uuid.uuid4().int
-
-    lower_bound, upper_bound = min(lower_bound, upper_bound), max(lower_bound, upper_bound)
-
-    res = clamp_array_to_bounds(random_numbers, lower_bound, upper_bound)
-
-    print(lower_bound, upper_bound)
-    print(random_numbers)
-    print(res)
-
-    assert all(lower_bound <= num <= upper_bound for num in res)
+def test_empty():
+    original_list = []
+    result_list = clamp_array_to_bounds(original_list, -10, 10)
+    expected_list = []
+    assert result_list == expected_list
 
 
-if __name__ == "__main__":
-    test_clamp_array_to_bounds()
+def test_lower_bound():
+    original_list = [1, 2, 3, 4, 5]
+    result_list = clamp_array_to_bounds(original_list, 3, 10)
+    expected_list = [3, 3, 3, 4, 5]
+    assert result_list == expected_list
+
+
+def test_upper_bound():
+    original_list = [1, 2, 3, 4, 5]
+    result_list = clamp_array_to_bounds(original_list, -10, 3)
+    expected_list = [1, 2, 3, 3, 3]
+    assert result_list == expected_list
+
+
+def test_invalid_args():
+    original_list = [1, 2, 3, 4, 5]
+    with pytest.raises(ValueError):
+        clamp_array_to_bounds(original_list, 5, -5)
+
+
+def test_original_list_unchanged():
+    original_list = [1, 2, 3, 4, 5]
+    clamp_array_to_bounds(original_list, 1, 5)
+    assert original_list == [1, 2, 3, 4, 5]
