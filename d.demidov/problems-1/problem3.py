075121c60851f0279e368bf0e44f48f703df18c8
“Dmitriy <d.demidov@g.nsu.ru>
2026-04-30 18:06:06 +0700

    cahnge pytest to unittest

--- d.demidov/problems-1/problem3.py
+++ d.demidov/problems-1/problem3.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 from logging import root as logger
 
 
@@ -13,15 +14,23 @@ def collatz_conjecture_compute(num: int) -> str:
     return " -> ".join(numbers_chain)
 
 
-def test_collatz_conjecture_compute() -> None:
+class TestCollatzConjectureCompute(unittest.TestCase):
+    def test_collatz_conjecture_compute(self) -> None:
+        result = collatz_conjecture_compute(3)
+        self.assertEqual(result, "3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1")
+
+    def test_collatz_for_one(self) -> None:
+        result = collatz_conjecture_compute(1)
+        self.assertEqual(result, "1")
+
+
+def run_collatz_from_cli() -> None:
     try:
         input_num = 3 if len(sys.argv) < 2 else int(sys.argv[1])
-        res = collatz_conjecture_compute(input_num)
-        assert res.endswith(" -> 1")
-        print(res)
+        print(collatz_conjecture_compute(input_num))
     except Exception as err:
         logger.exception(f"Exception in collatz test: {err}")
 
 
 if __name__ == "__main__":
-    test_collatz_conjecture_compute()
+    run_collatz_from_cli()
