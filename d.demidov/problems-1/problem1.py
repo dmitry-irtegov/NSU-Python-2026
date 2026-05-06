ea392557384b119b4d54776a3aed5e64dc4c0d3e
“Dmitriy <d.demidov@g.nsu.ru>
2026-05-07 00:32:25 +0700

    remove pytest add unittest

--- d.demidov/problems-1/problem1.py
+++ d.demidov/problems-1/problem1.py
@@ -1,22 +1,36 @@
-import numpy as np
-import uuid
+import unittest
+from collections.abc import Sequence
 
 
-def cummulative_sum(nums: list[int | float]) -> list[int | float]:
-    res = [0]
+def cummulative_sum(nums: Sequence[int | float]) -> list[int | float]:
+    res: list[int | float] = [0]
 
+    num: int | float
     for num in nums:
         res.append(res[-1] + num)
 
     return res
 
 
-def test_cummulative_sum() -> None:
-    print(cummulative_sum([1, 2, 3]))
-    random_numbers = [uuid.uuid4().int for _ in range(10)]
+class TestCummulativeSum(unittest.TestCase):
+    def test_simple_case(self) -> None:
+        nums: list[int] = [1, 2, 3]
+        res: list[int | float] = cummulative_sum(nums)
+        expected: list[int] = [0, 1, 3, 6]
+        self.assertEqual(res, expected)
 
-    assert np.array_equal(cummulative_sum(random_numbers)[1:], np.cumsum(random_numbers))
+    def test_empty_list(self) -> None:
+        nums: list[int] = []
+        res: list[int | float] = cummulative_sum(nums)
+        expected: list[int] = [0]
+        self.assertEqual(res, expected)
+
+    def test_mixed_values(self) -> None:
+        nums: list[int | float] = [2, -1, 0.5]
+        res: list[int | float] = cummulative_sum(nums)
+        expected: list[float] = [0, 2, 1, 1.5]
+        self.assertEqual(res, expected)
 
 
 if __name__ == "__main__":
-    test_cummulative_sum()
+    unittest.main()
