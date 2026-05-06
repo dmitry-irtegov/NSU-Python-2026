d65fc38cf6363156d61186d38757ebcb6b6aa5b4
“Dmitriy <d.demidov@g.nsu.ru>
2026-05-07 00:39:08 +0700

    remove pytest add unittest

--- d.demidov/problems-1/problem2.py
+++ d.demidov/problems-1/problem2.py
@@ -1,12 +1,17 @@
 import unittest
 
 
-def clamp_array_to_bounds(arr, lower_bound, upper_bound):
+def clamp_array_to_bounds(
+        arr: list[int | float],
+        lower_bound: int | float,
+        upper_bound: int | float,
+) -> list[int | float]:
     if lower_bound > upper_bound:
         raise ValueError("Lower bound must be less than upper bound")
 
-    result = []
+    result: list[int | float] = []
 
+    x: int | float
     for x in arr:
         if x > upper_bound:
             result.append(upper_bound)
@@ -18,30 +23,31 @@ def clamp_array_to_bounds(arr, lower_bound, upper_bound):
 
 
 class TestClampArrayToBounds(unittest.TestCase):
-    def test_empty(self):
-        original_list = []
-        result_list = clamp_array_to_bounds(original_list, -10, 10)
-        expected_list = []
+    def test_empty(self) -> None:
+        original_list: list[int | float] = []
+        result_list: list[int | float] = clamp_array_to_bounds(original_list, -10, 10)
+        expected_list: list[int | float] = []
         self.assertEqual(result_list, expected_list)
 
-    def test_lower_bound(self):
-        original_list = [1, 2, 3, 4, 5]
-        result_list = clamp_array_to_bounds(original_list, 3, 10)
-        expected_list = [3, 3, 3, 4, 5]
+    def test_lower_bound(self) -> None:
+        original_list: list[int | float] = [1, 2, 3, 4, 5]
+        result_list: list[int | float] = clamp_array_to_bounds(original_list, 3, 10)
+        expected_list: list[int | float] = [3, 3, 3, 4, 5]
         self.assertEqual(result_list, expected_list)
 
-    def test_upper_bound(self):
-        original_list = [1, 2, 3, 4, 5]
-        result_list = clamp_array_to_bounds(original_list, -10, 3)
-        expected_list = [1, 2, 3, 3, 3]
+    def test_upper_bound(self) -> None:
+        original_list: list[int | float] = [1, 2, 3, 4, 5]
+        result_list: list[int | float] = clamp_array_to_bounds(original_list, -10, 3)
+        expected_list: list[int | float] = [1, 2, 3, 3, 3]
         self.assertEqual(result_list, expected_list)
 
-    def test_invalid_args(self):
-        original_list = [1, 2, 3, 4, 5]
+    def test_invalid_args(self) -> None:
+        original_list: list[int | float] = [1, 2, 3, 4, 5]
         with self.assertRaises(ValueError):
             clamp_array_to_bounds(original_list, 5, -5)
 
-    def test_original_list_unchanged(self):
-        original_list = [1, 2, 3, 4, 5]
+    def test_original_list_unchanged(self) -> None:
+        original_list: list[int | float] = [1, 2, 3, 4, 5]
         clamp_array_to_bounds(original_list, 1, 5)
-        self.assertEqual(original_list, [1, 2, 3, 4, 5])
+        expected: list[int | float] = [1, 2, 3, 4, 5]
+        self.assertEqual(original_list, expected)
