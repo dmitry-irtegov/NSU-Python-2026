3495d1cee57ffe1571b88ee50e3d349558ab5b7a
“Dmitriy <d.demidov@g.nsu.ru>
2026-05-07 00:52:29 +0700

    annotations

--- d.demidov/problems-1/problem4.py
+++ d.demidov/problems-1/problem4.py
@@ -1,30 +1,48 @@
 import unittest
 
 
-def sing_the_song():
-    numbers = ("no", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+def sing_the_song() -> list[str]:
+    numbers: list[str] = [
+        "no",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+    ]
 
-    song_lines = []
+    song_lines: list[str] = []
 
-    bottles_number = 10
+    bottles_number: int = 10
     while bottles_number > 0:
-        first_line = f"{numbers[bottles_number].capitalize()} green bottle{'s' if bottles_number > 1 else ''} hanging on the wall,"
+        first_line: str = (
+            f"{numbers[bottles_number].capitalize()} green bottle{'s' if bottles_number > 1 else ''} hanging on the wall,"
+        )
         song_lines.append(first_line)
         song_lines.append(first_line)
 
-        third_line = f"{'And if' if bottles_number > 1 else 'If that'} one green bottle should accidentally fall,"
+        third_line: str = (
+            f"{'And if' if bottles_number > 1 else 'If that'} one green bottle should accidentally fall,"
+        )
         song_lines.append(third_line)
 
         bottles_number -= 1
 
-        fourth_line = f"There'll be {numbers[bottles_number]} green bottle{'s' if bottles_number != 1 else ''} hanging on the wall."
+        fourth_line: str = (
+            f"There'll be {numbers[bottles_number]} green bottle{'s' if bottles_number != 1 else ''} hanging on the wall."
+        )
         song_lines.append(fourth_line)
     return song_lines
 
 
 class TestSingTheSong(unittest.TestCase):
-    def test_song_equals_expected_text(self):
-        expected = (
+    def test_song_equals_expected_text(self) -> None:
+        expected: str = (
             "Ten green bottles hanging on the wall,\n"
             "Ten green bottles hanging on the wall,\n"
             "And if one green bottle should accidentally fall,\n"
@@ -67,10 +85,12 @@ class TestSingTheSong(unittest.TestCase):
             "There'll be no green bottles hanging on the wall."
         )
 
-        self.assertEqual("\n".join(sing_the_song()), expected)
+        result: str = "\n".join(sing_the_song())
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
-    song = sing_the_song()
+    song: list[str] = sing_the_song()
+    line: str
     for line in song:
         print(line)
