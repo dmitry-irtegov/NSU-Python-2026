6006f25f07961e35a93097364666e9f94ef8521c
Шебанов Иван <57228781+FortrainMas@users.noreply.github.com>
2026-05-07 16:44:32 +0700

    Refactor test cases with type hints and formatting
    Updated type hints for test methods and formatted expected values.

--- i.shebanov/problems-2/problem4/test.py
+++ i.shebanov/problems-2/problem4/test.py
@@ -5,49 +5,49 @@ from unittest.mock import patch
 
 
 class TestOutput(unittest.TestCase):
-    
-    def test_example(self):
+
+    def test_example(self) -> None:
         with patch('sys.stdout', new=StringIO()) as mock_out:
             problem4.main("123")
-            
-            expected = (
+
+            expected: str = (
                 "Found 4185 results.\n"
                 "Positions: 1923 2937 2975 3891 6547...\n"
             )
             self.assertEqual(mock_out.getvalue(), expected)
-            
-    def test_zero_index(self):
+
+    def test_zero_index(self) -> None:
         with patch('sys.stdout', new=StringIO()) as mock_out:
             problem4.main("1415")
-            
-            expected = (
+
+            expected: str = (
                 "Found 424 results.\n"
                 "Positions: 0 6954 29135 45233 79686...\n"
             )
             self.assertEqual(mock_out.getvalue(), expected)
-            
-    def test_less_than_five(self):
+
+    def test_less_than_five(self) -> None:
         with patch('sys.stdout', new=StringIO()) as mock_out:
             problem4.main("123456")
-            
-            expected = (
+
+            expected: str = (
                 "Found 2 results.\n"
                 "Positions: 2458884 3735792\n"
             )
             self.assertEqual(mock_out.getvalue(), expected)
-    
-    def test_non_int_input(self):
+
+    def test_non_int_input(self) -> None:
         with patch('sys.stdout', new=StringIO()) as mock_out:
             problem4.main("string input")
-            
-            expected = "Found 0 results.\n"
+
+            expected: str = "Found 0 results.\n"
             self.assertEqual(mock_out.getvalue(), expected)
 
-    def test_no_results(self):
+    def test_no_results(self) -> None:
         with patch('sys.stdout', new=StringIO()) as mock_out:
             problem4.main("1234091374676")
-            
-            expected = "Found 0 results.\n"
+
+            expected: str = "Found 0 results.\n"
             self.assertEqual(mock_out.getvalue(), expected)
 
 
