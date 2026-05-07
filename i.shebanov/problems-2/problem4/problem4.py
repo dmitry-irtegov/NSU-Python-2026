05d638969a3dce9afdbaa688f5fd041aa6fb3545
Шебанов Иван <57228781+FortrainMas@users.noreply.github.com>
2026-05-07 16:30:48 +0700

    Add type hints to functions in problem4.py
    Refactor code to add type hints for better readability and maintainability.

--- i.shebanov/problems-2/problem4/problem4.py
+++ i.shebanov/problems-2/problem4/problem4.py
@@ -1,10 +1,10 @@
 from sys import stderr, argv
 
 def find_search(text: str, pattern: str) -> list[int]:
-    indexes = []
-    start = 0
+    indexes: list[int] = []
+    start: int = 0
     while 1:
-        cur_index = text.find(pattern, start)
+        cur_index: int = text.find(pattern, start)
         if cur_index == -1:
             break
         indexes.append(cur_index)
@@ -12,10 +12,10 @@ def find_search(text: str, pattern: str) -> list[int]:
     return indexes
 
 
-def result_line(found_indexes: list[int])->str:
-    length = len(found_indexes)
+def result_line(found_indexes: list[int]) -> str:
+    length: int = len(found_indexes)
     found_indexes = found_indexes if length < 5 else found_indexes[:5]
-    result = f"Found {length} results."
+    result: str = f"Found {length} results."
 
     if length == 0:
         return result
@@ -27,14 +27,14 @@ def result_line(found_indexes: list[int])->str:
     )
     return result
 
-def main(pattern: str):
+def main(pattern: str) -> None:
     try:
         with open("pi.txt", "r", encoding="utf-8") as file:
             file.read(2)
-            text = file.read()
+            text: str = file.read()
             text = text.replace("\n", "")
 
-            indexes = find_search(text, pattern)
+            indexes: list[int] = find_search(text, pattern)
 
             print(result_line(indexes))
     except OSError as err:
