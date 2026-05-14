6f6d96111e5d25be08b884e0f3beaded0e598288
Takumy <Talumy@duck.com>
2026-05-14 17:58:53 +0700

    fixed

--- i.pereverzev2/problems3/problem3-4.py
+++ i.pereverzev2/problems3/problem3-4.py
@@ -1,65 +1,75 @@
+import itertools
 import unittest
+import random
 
-class CartesianCounter:
+
+class CartesianProduct:
     def __init__(self, elements, n):
-        self.elements = list(elements)
-        self.n = n
-        self.indices = [0] * n
+        self._product_list = list(elements)
+        self._n = n
+        self._iterator = itertools.cycle(
+            itertools.product(self._product_list, repeat=self._n)
+        )
+        self._current = next(self._iterator)
 
     def get_current(self):
-        return tuple(self.elements[i] for i in self.indices)
+        return self._current
 
     def set_next(self):
-        for i in range(self.n - 1, -1, -1):
-            self.indices[i] += 1
-            if self.indices[i] < len(self.elements):
-                break
-            self.indices[i] = 0
+        self._current = next(self._iterator)
 
 
-class TestCartesianCounter(unittest.TestCase):
-    def test_basic_example(self):
-        c = CartesianCounter([1, 'a'], 2)
-        
+class TestCartesianProduct(unittest.TestCase):
+
+    def test_basic_logic(self):
+        c = CartesianProduct([1, "a"], 2)
         self.assertEqual(c.get_current(), (1, 1))
         c.set_next()
-        self.assertEqual(c.get_current(), (1, 'a'))
+        self.assertEqual(c.get_current(), (1, "a"))
         c.set_next()
-        self.assertEqual(c.get_current(), ('a', 1))
+        self.assertEqual(c.get_current(), ("a", 1))
         c.set_next()
-        self.assertEqual(c.get_current(), ('a', 'a'))
-
-    def test_cycling(self):
-        c = CartesianCounter([1, 'a'], 2)
-        
-        for _ in range(4):
-            c.set_next()
-            
+        self.assertEqual(c.get_current(), ("a", "a"))
+        c.set_next()
         self.assertEqual(c.get_current(), (1, 1))
 
-    def test_n_equals_1(self):
-        c = CartesianCounter(['x', 'y', 'z'], 1)
-        
-        self.assertEqual(c.get_current(), ('x',))
-        c.set_next()
-        self.assertEqual(c.get_current(), ('y',))
-        c.set_next()
-        self.assertEqual(c.get_current(), ('z',))
-        c.set_next()
-        self.assertEqual(c.get_current(), ('x',))
+    def test_random_elements(self):
+        random.seed(42)
+        for _ in range(5):
+            elements = list(set(random.randint(0, 100) for _ in range(5)))
+            n = random.randint(1, 3)
 
-    def test_n_equals_3(self):
-        c = CartesianCounter([0, 1], 3)
-        expected = [
-            (0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1),
-            (1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1)
-        ]
-        
-        for exp in expected:
-            self.assertEqual(c.get_current(), exp)
+            c = CartesianProduct(elements, n)
+            expected_first = tuple([elements[0]] * n)
+            self.assertEqual(c.get_current(), expected_first)
+
+    def test_large_product(self):
+        n = 12
+        elements = [0, 1]
+        total_combinations = len(elements) ** n
+
+        c = CartesianProduct(elements, n)
+
+        for _ in range(total_combinations - 1):
             c.set_next()
-            
-        self.assertEqual(c.get_current(), (0, 0, 0))
 
-if __name__ == '__main__':
+        self.assertEqual(c.get_current(), tuple([1] * n))
+
+        c.set_next()
+        self.assertEqual(c.get_current(), tuple([0] * n))
+
+    def test_single_element_set(self):
+        c = CartesianProduct(["only"], 5)
+        self.assertEqual(c.get_current(), ("only", "only", "only", "only", "only"))
+        c.set_next()
+        self.assertEqual(c.get_current(), ("only", "only", "only", "only", "only"))
+
+    def test_n_is_zero(self):
+        c = CartesianProduct([1, 2, 3], 0)
+        self.assertEqual(c.get_current(), ())
+        c.set_next()
+        self.assertEqual(c.get_current(), ())
+
+
+if __name__ == "__main__":
     unittest.main()
