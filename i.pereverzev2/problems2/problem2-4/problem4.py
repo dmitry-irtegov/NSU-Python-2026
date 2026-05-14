c747a48fb9108fff62f8c3ffbed5488538a0e5a7
Takumy <Talumy@duck.com>
2026-05-14 16:08:54 +0700

    launch fixed

--- i.pereverzev2/problems2/problem2-4/problem4.py
+++ i.pereverzev2/problems2/problem2-4/problem4.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def find_indices(text, seq):
@@ -15,8 +16,11 @@ def find_indices(text, seq):
 
 def run_search():
     try:
-        with open("pi.txt", "r") as f:
-            pi_digits = f.read().strip()
+        base_dir = os.path.dirname(__file__)
+        file_path = os.path.join(base_dir, "pi.txt")
+        
+        with open(file_path, "r", encoding="utf-8") as file:
+            pi_digits = file.read().strip()
 
         while True:
             print("Enter sequence to search for.")
